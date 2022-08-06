866b34e4db8438f00836ba7814db4fe516bfe5d7
Lucas Rodrigues Dias <86005352+lucasrd2k@users.noreply.github.com>
2022-08-06 17:45:10 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$symbian =  strpos($_SERVER['HTTP_USER_AGENT'],"Symbian");
+
+if ($iphone || $ipad || $android || $palmpre || $ipod || $berry || $symbian == true) {
+    header("location: mobile/");
+} else {
+    header("location: pc/");
+}
+?>
